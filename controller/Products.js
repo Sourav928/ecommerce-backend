866d16ed0a95cb89f1a4395edad2dfc565ded2f5
sourav.khandekar <sourav.khandekar@nimblework.com>
2025-06-14 14:35:59 +0530

    fetchAllProducts API added.

--- controller/Products.js
+++ controller/Products.js
@@ -16,41 +16,41 @@ exports.fetchAllProducts = async (req, res) => {
     //filter = {"category":["smartphone","laptops"]}
     //sort = {_sort:"price",_order:"desc"}
     //pagination = {_page:1,_limit=10}
+    //TODO: we have to try with multiple category
+    let condition = {}
+    let query = Product.find(condition);
+    let totalProductsQuery = Product.find(condition);
 
-    let filter = {};
-
-    let totalProductsQuery = {}
+    console.log(req.query.category);
 
     if (req.query.category) {
-        filter.category = req.query.category;
-        totalProductsQuery.category = req.query.category;
+        query = query.find({ category: { $in: req.query.category.split(',') } });
+        totalProductsQuery = totalProductsQuery.find({
+            category: { $in: req.query.category.split(',') },
+        });
     }
     if (req.query.brand) {
-        filter.brand = req.query.brand;
-        totalProductsQuery.brand = req.query.brand;
+        query = query.find({ brand: { $in: req.query.brand.split(',') } });
+        totalProductsQuery = totalProductsQuery.find({ brand: { $in: req.query.brand.split(',') } });
     }
-    
-    let query = Product.find(filter);
-    // totalProductsQuery = query.count();
+    if (req.query._sort && req.query._order) {
+        query = query.sort({ [req.query._sort]: req.query._order });
+    }
+
+    const totalDocs = await totalProductsQuery.count().exec();
+    console.log({ totalDocs });
 
     if (req.query._page && req.query._limit) {
         const pageSize = req.query._limit;
         const page = req.query._page;
-        query = await query.skip(pageSize * (page - 1)).limit(pageSize);
-    }
-
-    // const totalDocs = await totalProductsQuery.count().exec();
-    // console.log({totalDocs});
-
-    if (req.query._sort && req.query._order) {
-        query = await query.sort({ [req.query._sort]: req.query._order });
+        query = query.skip(pageSize * (page - 1)).limit(pageSize);
     }
 
     try {
-        const doc = await query.exec();
-        // res.set('X-Total-Count',totalDocs)
-        res.status(200).json(doc);
+        const docs = await query.exec();
+        res.set('X-Total-Count', totalDocs);
+        res.status(200).json(docs);
     } catch (err) {
-        res.status(400).json(err)
+        res.status(400).json(err);
     }
 }
